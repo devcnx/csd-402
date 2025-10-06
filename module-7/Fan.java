ecb2313086a95124698030430747abc350110690
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-10-05 22:13:03 -0500

    docs: add class documentation and reorganize file headers

--- module-7/Fan.java
+++ module-7/Fan.java
@@ -1,9 +1,18 @@
 /**
+ * Refactored Fan class for improved readability and consistent use of `this` reference.
  * 
+ * The Fan class models a fan with attributes for speed, on/off state, radius, and color. It provides
+ * constructors, accessor and mutator methods, and a display method that outputs details without
+ * using toString().
+ * 
+ * Course: CSD402 Java for Programmers
+ * Assignment: M7 Programming Assignment
  * 
  * @author Brittaney Perry-Morgan
  * @version 1.0
  * 
- * Course: CSD402 Java for Programmers
- * Assignment: M7 Programming Assignment
  */
+
+public class Fan {
+    
+}
--- module-7/TestFan.java
+++ module-7/TestFan.java
@@ -1,9 +1,10 @@
 /**
  * 
  * 
+ * Course: CSD402 Java for Programmers
+ * Assignment: M7 Programming Assignment
+ * 
  * @author Brittaney Perry-Morgan
  * @version 1.0
  * 
- * Course: CSD402 Java for Programmers
- * Assignment: M7 Programming Assignment
  */
