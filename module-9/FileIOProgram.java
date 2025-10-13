54e515d1fcf882f54454470c8798084cfe0b6380
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-10-12 19:12:49 -0500

    feat: add Java program files for exception handling and file I/O demonstrations

---
+++ module-9/FileIOProgram.java
@@ -0,0 +1,13 @@
+
+/**
+ * Program 2: File I/O
+ * 
+ * Demonstration of file I/O using a Scanner to read a file and a PrintWriter to
+ * write to a file.
+ * 
+ * Course: CSD402 Java for Programmers
+ * Assignment: M9 Programming Assignment
+ * 
+ * @author Brittaney Perry-Morgan
+ * @version 1.0
+ */
