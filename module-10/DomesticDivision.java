c1ed6f4bdd747dc0b26169df43623077da9947d2
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-10-12 20:44:02 -0500

    feat: implement DomesticDivision class with state tracking and display functionality

---
+++ module-10/DomesticDivision.java
@@ -0,0 +1,38 @@
+/**
+ * DomesticDivision
+ * 
+ * Represents a domestic division of a company.
+ * 
+ * Course: CSD402 Java for Programmers
+ * Assignment: M10 Programming Assignment
+ * 
+ * @author Brittaney Perry-Morgan
+ * @version 1.0
+ */
+public class DomesticDivision extends Division {
+    private String state;
+
+    /**
+     * Constructor for DomesticDivision.
+     * 
+     * @param divisionName  The name of the division
+     * @param accountNumber The account number for the division
+     * @param state         The state where the division is located
+     */
+    public DomesticDivision(String divisionName, int accountNumber, String state) {
+        super(divisionName, accountNumber);
+        this.state = state;
+    }
+
+    /**
+     * Displays domestic division information.
+     */
+    @Override
+    public void display() {
+        System.out.println("Domestic Division:");
+        System.out.println("  Division Name: " + getDivisionName());
+        System.out.println("  Account #: " + getAccountNumber());
+        System.out.println("  State: " + state);
+        System.out.println();
+    }
+}
